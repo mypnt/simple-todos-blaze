0f46f43d89c2f6580e4293c7a77b8fc77cc84511
Mydrie PENOT <mydrie.penot2020@campus-eni.fr>
2021-04-02 04:31:04 +0200

    tutoriel étape 1-5, sans persistance données

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,11 +1,34 @@
 import { Template } from 'meteor/templating';
-
+import { TasksCollection } from '/imports/api/TasksCollection';
 import './App.html';
 
-Template.body.helpers({
+/*Template.body.helpers({
     tasks: [
         {text: 'Suivre le tutoriel Meteor'},
         {text: 'S\'approprier coffeeScript, Jade et Stylus'},
         {text: 'Créer son propre projet pour se faire la main'},
     ],
+});*/
+
+Template.body.helpers({
+    tasks() {
+        return TasksCollection.find({}, { sort: { createdAt: -1 } });
+    },
 });
+
+Template.form.events({
+    "submit .task-form"(event) {
+        event.preventDefault();
+
+        const target = event.target;
+        const text = target.text.value;
+
+        TasksCollection.insert({
+            text,
+            createdAt: new Date(),
+        });
+
+        target.text.value ='';
+    }
+})
+
--- server/main.js
+++ server/main.js
@@ -1,5 +1,18 @@
 import { Meteor } from 'meteor/meteor';
+import { TasksCollection } from '/imports/api/TasksCollection';
+
+const insertTask = taskText => TasksCollection.insert({ text: taskText });
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    if (TasksCollection.find().count() === 0) {
+        [
+            'Installer Node',
+            'Créer un server',
+            'Suivre tutoriel Meteor / Blaze',
+            'Se familiariser avec Jade',
+            'Se familiariser avec CoffeeScript',
+            'Se familiariser avec Stylus',
+            'Créer son propre projet'
+        ].forEach(insertTask)
+    }
 });
